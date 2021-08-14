887afb617045474365c7ef76363a6e686fe43f2c
flcv <felipe.lcv@gmail.com>
2021-08-14 23:06:50 +1000

    Blender python script to export models as text

---
+++ blender_print_vertex_xyz.py
@@ -0,0 +1,10 @@
+
+# CREDIT TO https://b3d.interplanety.org/en/how-to-save-mesh-data-to-text/
+
+import bpy
+
+vertices = [(vert.co.x, vert.co.y, vert.co.z) for vert in bpy.context.object.data.vertices]
+ 
+faces = [[vert for vert in polygon.vertices] for polygon in bpy.context.object.data.polygons]
+ 
+print('verts = ' + str(vertices) + '\n' + 'faces = ' + str(faces) + '\n')
