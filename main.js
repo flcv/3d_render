0a7e7c08bad9e4120640c7adb739539804c61c29
flcv <felipe.lcv@gmail.com>
2021-08-16 23:26:16 +1000

    Allow for multiple key inputs at once

--- main.js
+++ main.js
@@ -13,7 +13,33 @@
 //      - PUT INTO RENDERER CLASS?
 
 window.onload = ()=>{
-    
+
+    /***********************************/
+//FROM GROWING WITH THE WEB .COM
+// const times = [];
+// let fps;
+
+// function refreshLoop() {  
+//   window.requestAnimationFrame(() => {
+//     const now = performance.now();
+//     while (times.length > 0 && times[0] <= now - 1000) {
+//       times.shift();
+//     }
+//     times.push(now);
+//     fps = times.length;
+//     if(gameStarted){ //ONLY RENDER IF THE GAME HAS STARTED
+//         renderer.font="12px Helvetica";
+//         renderer.fillStyle="#309930"
+//         renderer.fillText(fps +" FPS",1,11);
+//     }
+//     refreshLoop();
+//   });
+// }
+
+// refreshLoop();
+
+/***********************************/
+    const GAME_FPS = 15;
     const canvasElement = document.getElementById("mainCanvas");
     const CANVAS_WIDTH = 1280;
     const CANVAS_HEIGHT = 720;
@@ -58,97 +84,92 @@ window.onload = ()=>{
     var s1 = new Scene(p1, po1, po2, po3/*l1, l2, l3, p1*/);
     var c1 = new Camera(-CANVAS_WIDTH/2, CANVAS_WIDTH/2, -CANVAS_HEIGHT/2, CANVAS_HEIGHT/2, -500, 500, s1, canvasContext);
     //s1.add(l1);
+    var keysDown = new Set();
 
-    document.addEventListener("keydown", e=>{
-        console.log(1);
-        e.key = e.key.toLowerCase();
-        if(e.key=="ArrowUp"){    
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = rotate(s1.objectList[item], "x", x);
-                // console.log(s1.objectList[item].coords);
-            }
-            // p1.coords = rotate(p1, "z", x);
-            // l1.coords = rotate(l1, "y", x);
-        } else if(e.key=="ArrowDown"){    
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = rotate(s1.objectList[item], "x", -x);
-                // console.log(s1.objectList[item].coords);
-            }
-            // p1.coords = rotate(p1, "z", x);
-            // l1.coords = rotate(l1, "y", x);
-        } else if(e.key=="ArrowLeft"){    
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = rotate(s1.objectList[item], "y", -x);
-                // console.log(s1.objectList[item].coords);
-            }
-            // p1.coords = rotate(p1, "z", x);
-            // l1.coords = rotate(l1, "y", x);
-        } else if(e.key=="ArrowRight"){    
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = rotate(s1.objectList[item], "y", x);
-                // console.log(s1.objectList[item].coords);
-            }
-            // p1.coords = rotate(p1, "z", x);
-            // l1.coords = rotate(l1, "y", x);
-        } else if(e.key=="r"){    
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = rotate(s1.objectList[item], "z", -x);
-                // console.log(s1.objectList[item].coords);
-            }
-            // p1.coords = rotate(p1, "z", x);
-            // l1.coords = rotate(l1, "y", x);
-        } else if(e.key=="t"){    
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = rotate(s1.objectList[item], "z", x);
-                // console.log(s1.objectList[item].coords);
-            }
-            // p1.coords = rotate(p1, "z", x);
-            // l1.coords = rotate(l1, "y", x);
-        } else if(e.key=="w"){
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = translate(s1.objectList[item], {coords: {x:0,y:-translationSpeed,z:0}, coords2: {x:0,y:-translationSpeed,z:0}});
-                // console.log(s1.objectList[item].coords);
-            }
-            
-        } else if(e.key=="a"){
-            // p1.coords = translate(p1, {coords: {x:-translationSpeed,y:0,z:0}});
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = translate(s1.objectList[item], {coords: {x:-translationSpeed,y:0,z:0}, coords2: {x:-translationSpeed,y:0,z:0}});
-                // console.log(s1.objectList[item].coords);
-            }
-        } else if(e.key=="s"){
-            //p1.coords = translate(p1, {coords: {x:0,y:translationSpeed,z:0}});
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = translate(s1.objectList[item], {coords: {x:0,y:translationSpeed,z:0}, coords2: {x:0,y:translationSpeed,z:0}});
-                // console.log(s1.objectList[item].coords);
-            }
-        } else if(e.key=="d"){
-            //p1.coords = translate(p1, {coords: {x:translationSpeed,y:0,z:0}});
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = translate(s1.objectList[item], {coords: {x:translationSpeed,y:0,z:0}, coords2: {x:translationSpeed,y:0,z:0}});
-                // console.log(s1.objectList[item].coords);
-            }
-        } else if(e.key=="q"){
-            //p1.coords = translate(p1, {coords: {x:0,y:0,z:translationSpeed}});
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = translate(s1.objectList[item], {coords: {x:0,y:0,z:translationSpeed}, coords2: {x:0,y:0,z:translationSpeed}});
-                // console.log(s1.objectList[item].coords);
-            }
-        } else if(e.key=="e"){
-            //p1.coords = translate(p1, {coords: {x:0,y:0,z:-translationSpeed}});
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = translate(s1.objectList[item], {coords: {x:0,y:0,z:-translationSpeed}, coords2: {x:0,y:0,z:-translationSpeed}});
-                // console.log(s1.objectList[item].coords);
-            }
-        } 
+    function main(){
 
+        
+        document.addEventListener("keydown", e=>{
+            console.log(1);
+            //e.key = e.key.toLowerCase();
+            keysDown.add(e.key);
+        });
+        document.addEventListener("keyup", e=>{
+            console.log(2);
+            //e.key = e.key.toLowerCase();
+            keysDown.delete(e.key);
+        });
+
+        handleInput(keysDown);
+        
         canvasContext.clearRect(0,0,1280,720);
         canvasContext.fillStyle = "#000";
         canvasContext.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
         // drawPoint(p1.coords, "rgba(255,0,0,1)", canvasContext);
         // drawPoint(p2.coords, "rgba(0,255,0,1)", canvasContext);
         c1.drawScene();
-    });
+    }
+
+    setInterval(main, 1000/GAME_FPS);
+
+
+    function handleInput(keysDown){
+        if(keysDown.has("ArrowUp")){    
+            for(let item in s1.objectList){
+                s1.objectList[item].coords = rotate(s1.objectList[item], "x", x);
+            }
+        } else if(keysDown.has("ArrowDown")){    
+            for(let item in s1.objectList){
+                s1.objectList[item].coords = rotate(s1.objectList[item], "x", -x);
+            }
+        }
+        if(keysDown.has("ArrowLeft")){    
+            for(let item in s1.objectList){
+                s1.objectList[item].coords = rotate(s1.objectList[item], "y", -x);
+            }
+        } else if(keysDown.has("ArrowRight")){    
+            for(let item in s1.objectList){
+                s1.objectList[item].coords = rotate(s1.objectList[item], "y", x);
+            }
+        } 
+        if(keysDown.has("r")){    
+            for(let item in s1.objectList){
+                s1.objectList[item].coords = rotate(s1.objectList[item], "z", -x);
+            }
+        } else if(keysDown.has("t")){    
+            for(let item in s1.objectList){
+                s1.objectList[item].coords = rotate(s1.objectList[item], "z", x);
+            }
+        }
+        if(keysDown.has("w")){
+            for(let item in s1.objectList){
+                s1.objectList[item].coords = translate(s1.objectList[item], {coords: {x:0,y:-translationSpeed,z:0}, coords2: {x:0,y:-translationSpeed,z:0}});
+            }
+        } else if(keysDown.has("s")){
+            for(let item in s1.objectList){
+                s1.objectList[item].coords = translate(s1.objectList[item], {coords: {x:0,y:translationSpeed,z:0}, coords2: {x:0,y:translationSpeed,z:0}});
+            }
+        }
+         if(keysDown.has("a")){
+            for(let item in s1.objectList){
+                s1.objectList[item].coords = translate(s1.objectList[item], {coords: {x:-translationSpeed,y:0,z:0}, coords2: {x:-translationSpeed,y:0,z:0}});
+            }
+        }  else if(keysDown.has("d")){
+            for(let item in s1.objectList){
+                s1.objectList[item].coords = translate(s1.objectList[item], {coords: {x:translationSpeed,y:0,z:0}, coords2: {x:translationSpeed,y:0,z:0}});
+            }
+        } 
+        if(keysDown.has("q")){
+            for(let item in s1.objectList){
+                s1.objectList[item].coords = translate(s1.objectList[item], {coords: {x:0,y:0,z:translationSpeed}, coords2: {x:0,y:0,z:translationSpeed}});
+            }
+        } else if(keysDown.has("e")){
+            for(let item in s1.objectList){
+                s1.objectList[item].coords = translate(s1.objectList[item], {coords: {x:0,y:0,z:-translationSpeed}, coords2: {x:0,y:0,z:-translationSpeed}});
+            }
+        } 
+    }
+
 };
 
 var previousX, previousY = 0;
