2866f8d521f5bce8b76d2d0286583cc5cb8f657b
flcv <felipe.lcv@gmail.com>
2021-08-22 17:32:50 +1000

    Added z-projection and text rendering

--- graphics.js
+++ graphics.js
@@ -11,24 +11,65 @@ function draw(obj, targetCanvasContext=CanvasRenderingContext2D, {lineWidth=2, c
     } else if(obj instanceof Line){
         try {
             targetCanvasContext.beginPath();
-            targetCanvasContext.moveTo(obj.vertices.vertex1.x+DRAW_X_OFFSET, obj.vertices.vertex1.y+DRAW_Y_OFFSET);
-            targetCanvasContext.lineTo(obj.vertices.vertex2.x+DRAW_X_OFFSET, obj.vertices.vertex2.y+DRAW_Y_OFFSET);
+            let newX1 = convert3DcoordsToViewPlane2D([obj.vertices.vertex1.x, obj.vertices.vertex1.z]);
+            let newY1 = convert3DcoordsToViewPlane2D([obj.vertices.vertex1.y, obj.vertices.vertex1.z]);
+            let newX2 = convert3DcoordsToViewPlane2D([obj.vertices.vertex2.x, obj.vertices.vertex2.z]);
+            let newY2 = convert3DcoordsToViewPlane2D([obj.vertices.vertex2.y, obj.vertices.vertex2.z]);
+            targetCanvasContext.moveTo(newX1+DRAW_X_OFFSET, newY1+DRAW_Y_OFFSET);
+            targetCanvasContext.lineTo(newX2+DRAW_X_OFFSET, newY2+DRAW_Y_OFFSET);
+            // targetCanvasContext.moveTo(obj.vertices.vertex1.x+DRAW_X_OFFSET, obj.vertices.vertex1.y+DRAW_Y_OFFSET);
+            // targetCanvasContext.lineTo(obj.vertices.vertex2.x+DRAW_X_OFFSET, obj.vertices.vertex2.y+DRAW_Y_OFFSET);
             targetCanvasContext.lineWidth = lineWidth;
             targetCanvasContext.strokeStyle = colour;
             targetCanvasContext.stroke();
         } catch(e){ console.error(e); }
     } else if(obj instanceof Polygon){
         try {
+            let newX1 = convert3DcoordsToViewPlane2D([obj.vertices.vertex1.x, obj.vertices.vertex1.z]);
+            let newY1 = convert3DcoordsToViewPlane2D([obj.vertices.vertex1.y, obj.vertices.vertex1.z]);
+            let newX2 = convert3DcoordsToViewPlane2D([obj.vertices.vertex2.x, obj.vertices.vertex2.z]);
+            let newY2 = convert3DcoordsToViewPlane2D([obj.vertices.vertex2.y, obj.vertices.vertex2.z]);
+            let newX3 = convert3DcoordsToViewPlane2D([obj.vertices.vertex3.x, obj.vertices.vertex3.z]);
+            let newY3 = convert3DcoordsToViewPlane2D([obj.vertices.vertex3.y, obj.vertices.vertex3.z]);
             targetCanvasContext.beginPath();
-            targetCanvasContext.moveTo(obj.vertices.vertex1.x+DRAW_X_OFFSET, obj.vertices.vertex1.y+DRAW_Y_OFFSET);
-            targetCanvasContext.lineTo(obj.vertices.vertex2.x+DRAW_X_OFFSET, obj.vertices.vertex2.y+DRAW_Y_OFFSET);
-            targetCanvasContext.lineTo(obj.vertices.vertex3.x+DRAW_X_OFFSET, obj.vertices.vertex3.y+DRAW_Y_OFFSET);
-            targetCanvasContext.lineTo(obj.vertices.vertex1.x+DRAW_X_OFFSET, obj.vertices.vertex1.y+DRAW_Y_OFFSET);
+            targetCanvasContext.moveTo(newX1+DRAW_X_OFFSET, newY1+DRAW_Y_OFFSET);
+            targetCanvasContext.lineTo(newX2+DRAW_X_OFFSET, newY2+DRAW_Y_OFFSET);
+            targetCanvasContext.lineTo(newX3+DRAW_X_OFFSET, newY3+DRAW_Y_OFFSET);
+            targetCanvasContext.lineTo(newX1+DRAW_X_OFFSET, newY1+DRAW_Y_OFFSET);
+            // targetCanvasContext.moveTo(obj.vertices.vertex1.x+DRAW_X_OFFSET, obj.vertices.vertex1.y+DRAW_Y_OFFSET);
+            // targetCanvasContext.lineTo(obj.vertices.vertex2.x+DRAW_X_OFFSET, obj.vertices.vertex2.y+DRAW_Y_OFFSET);
+            // targetCanvasContext.lineTo(obj.vertices.vertex3.x+DRAW_X_OFFSET, obj.vertices.vertex3.y+DRAW_Y_OFFSET);
+            // targetCanvasContext.lineTo(obj.vertices.vertex1.x+DRAW_X_OFFSET, obj.vertices.vertex1.y+DRAW_Y_OFFSET);
             targetCanvasContext.lineWidth = 0.5;
             targetCanvasContext.strokeStyle = colour;
             targetCanvasContext.stroke();
         } catch(e){ console.error(e); }
     }
+}
+
+/**
+ * Performs a projection of a point onto a view plane a certain distance away
+ * @param {Array} coords [x,z] or [y,z]
+ * @param {Number} viewPlaneDistance Distance of the view plane from the camera
+ * @returns Number; x or y on the view plane
+ */
+ function convert3DcoordsToViewPlane2D(coords = [0,0], viewPlaneDistance = 10, cameraMagnificationValue=10){
+    return ((coords[0]*(viewPlaneDistance/coords[1]))*cameraMagnificationValue);
+}
+
+function drawText(targetCanvasContext, {font="12px Consolas", color="#00FF00", x=0, y=0, text=`Null text`, lineHeight=11}){
+    try {
+        var newText = text.split("\n"); //SUPPORT FOR NEW LINES, SUCH AS IN `ABC\nDEF` OR ACTUAL NEWLINES IN A GIVEN CODE EDITOR
+        targetCanvasContext.font=font;
+        targetCanvasContext.fillStyle=color;
+        for(let lineIndex in newText){
+            targetCanvasContext.fillText(newText[lineIndex],x,y+lineHeight*lineIndex);
+        }
+    } catch(e) {
+        console.error(e);
+        return false;
+    }
+    return true;
 }
 
 class Scene{
