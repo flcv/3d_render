bae193244f68b0128d309b8e414614c50840c995
flcv <felipe.lcv@gmail.com>
2021-08-22 17:32:24 +1000

    Massively improved input handling and refresh loop (smoother, +fps counter)

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 //TO-DO:
+// - TRANSLATE CODE CAN BE COMPLETELY COLLAPSED INTO 1 OR 2 LOC, GIVEN SMALL CHANGES TO POINT AND LINE CLASS
+// - PLAY AROUND WITH CAMERA MAGNIFICATION, VIEW PLANE DISTANCE, NEAREST AND FURTHEST Z VALUES TO GET PERFECT RENDERING
+//      - HAVE THEM WITHIN THE CAMERA CLASS?
 // - LOCAL VS GLOBAL TRANSFORMATIONS
-// - WEIRD BUG; REFRESH, HOLD ARROWDOWN -> MONKEY FACING FORWARD, REPEAT -> MONKEY FACING BACKWARDS
-//      - ONLY HAPPENS SOMETIMES, OTHER MODELS AFFECTED? OR BUG ON MONKEY MAKES IT APPEAR AS IF OTHER MODELS AFFECTED
 // - REWORK VERTEX ID SYSTEM
 // - ADD MOUSE ROTATION (ARROW KEYS FEELS VERY UNNATURAL)
 // - MOVE MODEL ROTATION CODE FROM handleInput() TO rotate() FUNCTION AND MODEL set coords/vertices FUNCS
@@ -17,46 +18,24 @@
 
 window.onload = ()=>{
 
-    /***********************************/
-//FROM GROWING WITH THE WEB .COM
-// const times = [];
-// let fps;
 
-// function refreshLoop() {  
-//   window.requestAnimationFrame(() => {
-//     const now = performance.now();
-//     while (times.length > 0 && times[0] <= now - 1000) {
-//       times.shift();
-//     }
-//     times.push(now);
-//     fps = times.length;
-//     if(gameStarted){ //ONLY RENDER IF THE GAME HAS STARTED
-//         renderer.font="12px Helvetica";
-//         renderer.fillStyle="#309930"
-//         renderer.fillText(fps +" FPS",1,11);
-//     }
-//     refreshLoop();
-//   });
-// }
-
-// refreshLoop();
-
-/***********************************/
-    const GAME_FPS = 60;
     const canvasElement = document.getElementById("mainCanvas");
     const CANVAS_WIDTH = 1280;
     const CANVAS_HEIGHT = 720;
     canvasElement.width = CANVAS_WIDTH;
     canvasElement.height = CANVAS_HEIGHT;
     const canvasContext = canvasElement.getContext("2d");
+    const instructionsText = `=====================\nW, A, S, D, Q, E -> move (translation)\nArrow Keys, T, R -> rotate`;
     
-    var x=3;
+    const nearestZ = 30; //NEAREST AND FURTHEST THE CAMERA CAN SEE BEFORE THE POLYGON IS CULLED
+    const furthestZ = 500; //BOTH WERE CHOSEN ARBITRARILY
+    const rotationSpeed=3;
     const translationSpeed = 10;
     var p1 = new Point(0,0,0);
     // var p2 = new Point(150,0,150);
 
 
-    var v1 = new Vector3(0,0,0);
+    var v1 = new Vector3(0,0,furthestZ);
     var v2 = new Vector3(0,300,0);
     // var p3 = new Point(-100,-100,0);
     // var p4 = new Point(100,-100,0);
@@ -69,6 +48,7 @@ window.onload = ()=>{
     // var m1 = new Model(po1, po2, po3, po4);
     var m1 = createModelFromBlender(model_blenderMonkey.verts, model_blenderMonkey.faces, v1);
     var m2 = createModelFromBlender(model_wonkyTetrahedron.verts, model_wonkyTetrahedron.faces, v2);
+    var m3 = createModelFromBlender(model_cube.verts, model_cube.faces, v1);
 
     // var v2 = new Vector3(50,50,0);
     // var p6 = new Point(10,-10,-10);
@@ -86,130 +66,132 @@ window.onload = ()=>{
     var p6 = new Point(0,-200,0);
     var p7 = new Point(0,0,0);
     var p8 = new Point(0,0,200);*/
-    //var l1 = new Line(p3,p4,v1);
+    var l1 = new Line(new Point(0,0,0),new Point(100,0,0),v1); //REPRESENTATION OF X AXIS
+    var l2 = new Line(new Point(0,0,0),new Point(0,100,0),v1); //REPRESENTATION OF Y AXIS
+    var l3 = new Line(new Point(0,0,0),new Point(0,0,100),v1); //REPRESENTATION OF Z AXIS
+    var staticElementsUnder = [l1,l2,l3];
     
+    var boundingBoxB = new Line(new Point(-200,200,0),new Point(300,200,0),v1); //EXAMPLE OF SOMETHING RENDERED OVER CAMERA'S SCENE
+    var boundingBoxR = new Line(new Point(300,200,0),new Point(300,-200,0),v1); 
+    var boundingBoxT = new Line(new Point(300,-200,0),new Point(-200,-200,0),v1); 
+    var boundingBoxL = new Line(new Point(-200,-200,0),new Point(-200,200,0),v1); 
+    var staticElementsOver = [boundingBoxB,boundingBoxL,boundingBoxR,boundingBoxT];
+
     // po1.vertices = centreAtOrigin(po1);
     // po2.vertices = centreAtOrigin(po2);
     // console.log(po1.centre);
     // console.log(centreAtOrigin(po1));
     /*var l2 = new Line(p5,p6);
     var l3 = new Line(p7,p8);*/
-    var s1 = new Scene(p1, m1, m2 /*po1, po2, po3, po4*//*l1, l2, l3, p1*/);
-    var c1 = new Camera(-CANVAS_WIDTH/2, CANVAS_WIDTH/2, -CANVAS_HEIGHT/2, CANVAS_HEIGHT/2, -500, 500, s1, canvasContext);
+    var s1 = new Scene(p1, m1, m2, m3/*po1, po2, po3, po4*//*l1, l2, l3, p1*/);
+    var c1 = new Camera(/*-CANVAS_WIDTH/2*/-200, /*CANVAS_WIDTH/2*/300, /*-CANVAS_HEIGHT/2*/-200, /*CANVAS_HEIGHT/2*/200, nearestZ, furthestZ, s1, canvasContext);
     //s1.add(l1);
     var keysDown = new Set();
+    var fps = 0;
+    var oldTime = 0;
+    window.requestAnimationFrame(main);
 
-    function main(){
-
+    function main(currentTime){
+        //FPS COUNTER
+        fps = Math.floor(1000/(currentTime - oldTime));
+        oldTime = currentTime;
         
+
+        //INPUT
         document.addEventListener("keydown", e=>{
-            console.log(1);
+            //console.log(1);
             //e.key = e.key.toLowerCase();
-            keysDown.add(e.key);
+            keysDown.add(e.key.toLowerCase());
         });
         document.addEventListener("keyup", e=>{
-            console.log(2);
+            //console.log(2);
             //e.key = e.key.toLowerCase();
-            keysDown.delete(e.key);
+            keysDown.delete(e.key.toLowerCase());
         });
-
         handleInput(keysDown);
         
+        //RENDERING
         canvasContext.clearRect(0,0,1280,720);
         canvasContext.fillStyle = "#000";
         canvasContext.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-        // drawPoint(p1.coords, "rgba(255,0,0,1)", canvasContext);
-        // drawPoint(p2.coords, "rgba(0,255,0,1)", canvasContext);
+        drawText(canvasContext, {x: 1, y: 10, text:`${fps} FPS`}); //FPS
+        drawText(canvasContext, {x: 1, y: 20, text:instructionsText}); //INSTRUCTIONS
+        for(let element of staticElementsUnder){
+            draw(element, canvasContext, {colour: "#444"});
+        }
         c1.drawScene();
+        for(let element of staticElementsOver){
+            draw(element, canvasContext, {colour: "#BBB"});
+        }
+        
+        
+        //REFRESH
+        window.requestAnimationFrame(main);
     }
 
-    setInterval(main, 1000/GAME_FPS);
 
 
     function handleInput(keysDown){
-        if(keysDown.has("ArrowUp")){    
-            for(let item in s1.objectList){
-                if(s1.objectList[item] instanceof Model){
-                    for(let poly of s1.objectList[item].polygonsList){
-                        poly.coords = rotate(poly, "x", x);
-                    }
-                } else {
-                    s1.objectList[item].coords = rotate(s1.objectList[item], "x", x);
-                }
-            }
-        } else if(keysDown.has("ArrowDown")){    
-            for(let item in s1.objectList){
-                if(s1.objectList[item] instanceof Model){
-                    for(let poly of s1.objectList[item].polygonsList){
-                        poly.coords = rotate(poly, "x", -x);
-                    }
-                } else {
-                    s1.objectList[item].coords = rotate(s1.objectList[item], "x", -x);
-                }
-            }
+        var rotationQueue = [];
+        if(keysDown.has("arrowup")){    
+            rotationQueue.push(["x", rotationSpeed]);
+        } else if(keysDown.has("arrowdown")){    
+            rotationQueue.push(["x", -rotationSpeed]);
         }
-        if(keysDown.has("ArrowLeft")){    
-            for(let item in s1.objectList){
-                if(s1.objectList[item] instanceof Model){
-                    for(let poly of s1.objectList[item].polygonsList){
-                        poly.coords = rotate(poly, "y", -x);
-                    }
-                } else {
-                    s1.objectList[item].coords = rotate(s1.objectList[item], "y", -x);
-                }
-            }
-        } else if(keysDown.has("ArrowRight")){    
-            for(let item in s1.objectList){
-                if(s1.objectList[item] instanceof Model){
-                    for(let poly of s1.objectList[item].polygonsList){
-                        poly.coords = rotate(poly, "y", x);
-                    }
-                } else {
-                    s1.objectList[item].coords = rotate(s1.objectList[item], "y", x);
-                }
-            }
+        if(keysDown.has("arrowleft")){    
+            rotationQueue.push(["y", -rotationSpeed]);
+        } else if(keysDown.has("arrowright")){    
+            rotationQueue.push(["y", rotationSpeed]);
         } 
         if(keysDown.has("r")){    
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = rotate(s1.objectList[item], "z", -x);
-            }
+            rotationQueue.push(["z", -rotationSpeed]);
         } else if(keysDown.has("t")){    
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = rotate(s1.objectList[item], "z", x);
-            }
+            rotationQueue.push(["z", rotationSpeed]);
         }
+
+        rotationQueue.forEach(v=>{
+            for(let item in s1.objectList){
+                if(s1.objectList[item] instanceof Model){
+                    for(let poly of s1.objectList[item].polygonsList){
+                        poly.coords = rotate(poly, ...v);
+                    }
+                } else {
+                    s1.objectList[item].coords = rotate(s1.objectList[item], ...v);
+                }
+            }
+        });
+        
+
+        var translationOptions = {x: 0, y: 0, z: 0};
         if(keysDown.has("w")){
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = translate(s1.objectList[item], {coords: {x:0,y:-translationSpeed,z:0}, coords2: {x:0,y:-translationSpeed,z:0}});
-            }
+            translationOptions.y = -translationSpeed;
         } else if(keysDown.has("s")){
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = translate(s1.objectList[item], {coords: {x:0,y:translationSpeed,z:0}, coords2: {x:0,y:translationSpeed,z:0}});
-            }
+            translationOptions.y = translationSpeed;
         }
          if(keysDown.has("a")){
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = translate(s1.objectList[item], {coords: {x:-translationSpeed,y:0,z:0}, coords2: {x:-translationSpeed,y:0,z:0}});
-            }
+            translationOptions.x = -translationSpeed;
         }  else if(keysDown.has("d")){
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = translate(s1.objectList[item], {coords: {x:translationSpeed,y:0,z:0}, coords2: {x:translationSpeed,y:0,z:0}});
-            }
+            translationOptions.x = translationSpeed;
         } 
         if(keysDown.has("q")){
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = translate(s1.objectList[item], {coords: {x:0,y:0,z:translationSpeed}, coords2: {x:0,y:0,z:translationSpeed}});
-            }
+            translationOptions.z = translationSpeed;
         } else if(keysDown.has("e")){
-            for(let item in s1.objectList){
-                s1.objectList[item].coords = translate(s1.objectList[item], {coords: {x:0,y:0,z:-translationSpeed}, coords2: {x:0,y:0,z:-translationSpeed}});
-            }
+            translationOptions.z = -translationSpeed;
         } 
+        for(let item in s1.objectList){
+            if(s1.objectList[item] instanceof Model){
+                for(let poly of s1.objectList[item].polygonsList){
+                    poly.coords = translate(poly, {coords: translationOptions});
+                }
+            } else {
+                s1.objectList[item].coords = translate(s1.objectList[item], {coords: translationOptions});
+            }
+        }
     }
 
 };
 
-var previousX, previousY = 0;
+// var previousX, previousY = 0;
 // canvas.addEventListener("mousemove", (e)=>{ 
 //     //console.log(`x: ${e.offsetX - previousX} | y: ${e.offsetY - previousY}`);
     
